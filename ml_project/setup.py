eb38103ee06acd62b282e728652220fd27d6446c
unknown <chaudhari.chandan22@gmail.com>
2025-07-24 23:53:59 +0530

    Second Commit Done

---
+++ ml_project/setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="Medical Insurance Charges Prediction Model",
+    version="1.0.0",
+    description="This is Multivariate/Regression Analysis Problem",
+    author="Chandan Chaudhari",
+    author_email="chaudhari.chandan22@gmail.com",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=["numpy","pandas", "scikit-learn", "joblib", "mlflow","matplotlib","seaborn"],
+)
